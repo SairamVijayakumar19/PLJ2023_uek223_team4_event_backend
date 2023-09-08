ea721a257bb62d8b0c4d8ba3f544e9a296cdc36e
Noah8820 <noah.deboni@noseryoung.com>
2023-09-08 13:43:11 +0200

    adding authorityies to the backend project

--- backend/src/main/java/com/example/demo/domain/event/EventController.java
+++ backend/src/main/java/com/example/demo/domain/event/EventController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,11 +17,14 @@ public class EventController {
     @Autowired
     private EventService eventService;
 
+
+    @PreAuthorize("hasAuthority('EVENT_READ')")
     @GetMapping
     public ResponseEntity<List<Event>> getAllEvents() {
         return ResponseEntity.ok(eventService.getAllEvents());
     }
 
+    @PreAuthorize("hasAuthority('EVENT_READ')")
     @GetMapping("/{id}")
     public ResponseEntity<Event> getEventById(@PathVariable Integer id) {
         return eventService.getEventById(id)
@@ -28,11 +32,13 @@ public class EventController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @PreAuthorize("hasAuthority('EVENT_CREATE')")
     @PostMapping
     public ResponseEntity<Event> createEvent(@RequestBody Event event) {
         return ResponseEntity.ok(eventService.createEvent(event));
     }
 
+    @PreAuthorize("hasAuthority('EVENT_MODIFY')")
     @PutMapping("/{id}")
     public ResponseEntity<Event> updateEvent(@PathVariable Integer id, @RequestBody Event event) {
         Event updatedEvent = eventService.updateEvent(id, event);
@@ -42,12 +48,14 @@ public class EventController {
         return ResponseEntity.notFound().build();
     }
 
+    @PreAuthorize("hasAuthority('EVENT_DELETE')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteEvent(@PathVariable Integer id) {
         eventService.deleteEvent(id);
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasAuthority('EVENT_READ_PARTICIPANTS')")
     @GetMapping("/{id}/participants")
     public ResponseEntity<Page<User>> getEventParticipants(@PathVariable Integer id, Pageable pageable) {
         return ResponseEntity.ok(eventService.getEventParticipants(id, pageable));
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -21,25 +21,36 @@ VALUES ('2ebf301e-6c61-4076-98e3-2a38b31daf86', 'DEFAULT'),
 ('07e848fa-4d62-11ee-be56-0242ac120002', 'EVENT_POST'),
 ('07e85052-4d62-11ee-be56-0242ac120002', 'EVENT_DELETE'),
 ('07e851ba-4d62-11ee-be56-0242ac120002', 'EVENT_MODIFY'),
-('07e852fa-4d62-11ee-be56-0242ac120002', 'EVENT_JOIN')
-
-
+('efb7b108-4e38-11ee-be56-0242ac120002', 'EVENT_JOIN'),
+('6b30a440-4e37-11ee-be56-0242ac120002', 'EVENT_READ'),
+('6b30a896-4e37-11ee-be56-0242ac120002', 'EVENT_READ_PARTICIPANTS')
 ON CONFLICT DO NOTHING;
 
 --assign roles to users
 insert into users_role (users_id, role_id)
-values ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'd29e709c-0ff1-4f4c-a7ef-09f656c390f1'),
-       ('0d8fa44c-54fd-4cd0-ace9-2a7da57992de', 'd29e709c-0ff1-4f4c-a7ef-09f656c390f1'),
-       ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'ab505c92-7280-49fd-a7de-258e618df074'),
-       ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02')
+values
+       ('0d8fa44c-54fd-4cd0-ace9-2a7da57992de', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02'),
+       ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'ab505c92-7280-49fd-a7de-258e618df074')
  ON CONFLICT DO NOTHING;
 
---assign authorities to roles
+-- USER Role hat Authorities "EVENT_CREATE", "EVENT_MODIFY", "EVENT_DELETE", "EVENT_READ_PARTICIPANTS", "EVENT_READ" und EVENT_JOIN
 INSERT INTO role_authority(role_id, authority_id)
-VALUES ('d29e709c-0ff1-4f4c-a7ef-09f656c390f1', '2ebf301e-6c61-4076-98e3-2a38b31daf86'),
-('ab505c92-7280-49fd-a7de-258e618df074', '76d2cbf6-5845-470e-ad5f-2edb9e09a868'),
-('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '21c942db-a275-43f8-bdd6-d048c21bf5ab')
+VALUES
+    ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '76d2cbf6-5845-470e-ad5f-2edb9e09a868'),
+    ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '21c942db-a275-43f8-bdd6-d048c21bf5ab'),
+    ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '07e848fa-4d62-11ee-be56-0242ac120002'),
+    ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '6b30a896-4e37-11ee-be56-0242ac120002'),
+    ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '6b30a440-4e37-11ee-be56-0242ac120002'),
+    ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', 'efb7b108-4e38-11ee-be56-0242ac120002')
  ON CONFLICT DO NOTHING;
+
+-- ADMIN hat "EVENT_READ",
+INSERT INTO role_authority(role_id, authority_id)
+VALUES
+    ('ab505c92-7280-49fd-a7de-258e618df074', '6b30a440-4e37-11ee-be56-0242ac120002'),
+    ('ab505c92-7280-49fd-a7de-258e618df074', '6b30a896-4e37-11ee-be56-0242ac120002'),
+    ('ab505c92-7280-49fd-a7de-258e618df074', '07e848fa-4d62-11ee-be56-0242ac120002')
+ON CONFLICT DO NOTHING;
 
 INSERT INTO event (event_name, date, location)
 VALUES ('Summer Party', '2023-06-15', 'Beach Club'),
